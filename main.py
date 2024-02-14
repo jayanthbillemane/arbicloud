2c7bcd9a401a6db5baf77de4a04e5c294388c906
Ubuntu <azureuser@arvm.z5yvsjqss34uvh135r1q2voahd.rx.internal.cloudapp.net>
2024-02-14 05:57:48 +0000

    new update

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ app = FastAPI()
 # CORS middleware with specific origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://example.com"],
+    allow_origins=["http://example.com", "http://localhost:30"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# def validate_origin(origin: str = Header(...)):
+#     if origin != "https://www.arbi.cloud/" or:
+#         raise HTTPException(status_code=403, detail="Forbidden")
+
 # Define a route for the root endpoint "/"
-@app.get("/")
+@app.get("/test")
 def read_root():
     data = [{"id": 1, "name": "Jayantha BS", "Profession": "Fullstack Developer", "Experience": "5 Years",
             "company": ["EFI", "Risk Advisors Inc", "Medilenz", "CamcomAI"]
